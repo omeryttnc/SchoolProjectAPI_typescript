dab6f0f82e37baec903ebbe8067be57a78348602
omeryttnc <omeryttnc@gmail.com>
2024-11-01 11:45:03 +0000

    secKey

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,8 @@
 import crypto from 'crypto';
-import { secretdata } from '../../etc/secrets/secretdata';
+// import { secretdata } from '../../etc/secrets/secretdata';
 
 export const random=()=> crypto.randomBytes(128).toString('base64');
 export const authentication = (salt:string,password:string)=>{
-    return crypto.createHmac('sha256',[salt,password].join('/')).update(secretdata.secret).digest('hex')
+    const secKey = process.env.SECKEY || "Selam12_Nasil-34"
+    return crypto.createHmac('sha256',[salt,password].join('/')).update(secKey).digest('hex')
 }
