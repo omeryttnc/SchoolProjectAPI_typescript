81880eaf9b14f8212d610d0d7b42277d29178172
omeryttnc <omeryttnc@gmail.com>
2024-11-22 11:55:14 +0000

    environment

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,11 @@
-import crypto from 'crypto';
- import { secretdata } from '../../etc/secrets/secretdata';
+import crypto from "crypto";
+//import { secretdata } from '../../etc/secrets/secretdata';
 
-export const random=()=> crypto.randomBytes(128).toString('base64');
-export const authentication = (salt:string,password:string)=>{
-    const secKey = process.env.SECKEY || secretdata.secret
-    return crypto.createHmac('sha256',[salt,password].join('/')).update(secKey).digest('hex')
-}
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+  const secKey = process.env.SECRET || ""; //secretdata.secret
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(secKey)
+    .digest("hex");
+};
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import { secretdata } from "../etc/secrets/secretdata";
 import mongoose from "mongoose";
 
 import router from "./router";
 import { config } from "../config";
 
 const PORT = process.env.PORT || config.port;
-const DATABASE= process.env.DATABASE || secretdata.mongo
+const DATABASE = process.env.DATABASE || ""; //secretdata.mongo
 
 const app = express();
 
@@ -39,18 +38,18 @@ app.use("/api", router());
 
 //TODO CI/CD pipeline should be added
 //TODO jest superTest some test should be added
-//TODO cookies will give challange needs to find a way or change to barear token 
-//TODO lesson module should be added 
+//TODO cookies will give challange needs to find a way or change to barear token
+//TODO lesson module should be added
 //TODO admin should assign lesson to teacher
 //TODO we need secretary for some admin roles
 //TODO we need one super admin to approve admin. super admin should have additional password
 //TODO class module should be created
-//TODO secretary should assign lesson to teacher but it will be active when admin approve it 
+//TODO secretary should assign lesson to teacher but it will be active when admin approve it
 //TODO result module should be created
-//TODO teacher should add/update/delete results 
+//TODO teacher should add/update/delete results
 //TODO admin and students only can see resuls
-//TODO report module should created 
-//TODO secretary should create report according to 
+//TODO report module should created
+//TODO secretary should create report according to
 //TODO a_ teacher
 //TODO b_ student
 //TODO c_ subject
