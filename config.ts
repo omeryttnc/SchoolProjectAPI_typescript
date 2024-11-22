9b14f1c3b11a45080b29a99e520b5e436cbd4026
omeryttnc <omeryttnc@gmail.com>
2024-11-22 12:09:48 +0000

    READ ME update

--- config.ts
+++ config.ts
@@ -2,11 +2,9 @@ export const config = {
   port: 3000,
   baseUrl: "http://127.0.0.1:3000",
   jwtExpirationInSeconds: 60 * 60, // 1 hour
-
 };
-export const roles={
+export const roles = {
   ADMIN: "admin",
   STUDENT: "student",
-  TEACHER: "teacher"
+  TEACHER: "teacher",
 };
-
